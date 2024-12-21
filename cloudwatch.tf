b0d413f8c54803dcb59158534a5c051e141320d1
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-12-21 13:04:53 -0300

    chore: tf fmt

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,8 +1,8 @@
 resource "aws_cloudwatch_log_group" "lambda_auth_lg" {
-    name              = "/aws/lambda/ApiGatewayAuthenticator"
-    retention_in_days = 30
-  skip_destroy = false
+  name              = "/aws/lambda/ApiGatewayAuthenticator"
+  retention_in_days = 30
+  skip_destroy      = false
   tags = {
-    Name="/aws/lambda/ApiGatewayAuthenticator"
+    Name = "/aws/lambda/ApiGatewayAuthenticator"
   }
 }
