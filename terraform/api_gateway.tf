64164f9e46eff5004e30538e1b34beec9fafe6e2
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-12-23 20:43:17 -0300

    refactor: replace integration

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -1,16 +1,3 @@
-resource "aws_apigatewayv2_vpc_link" "vpc_link" {
-  name = "vpc-link"
-  subnet_ids = [
-    data.terraform_remote_state.geral.outputs.subnet_private_a_id,
-    data.terraform_remote_state.geral.outputs.subnet_private_b_id
-  ]
-  security_group_ids = [data.terraform_remote_state.geral.outputs.security_group_id]
-
-  tags = {
-    Name = "vpc-link"
-  }
-}
-
 resource "aws_apigatewayv2_api" "api_gateway" {
   name                         = "ambrosia-api-gateway"
   protocol_type                = "HTTP"
@@ -35,10 +22,9 @@ resource "aws_apigatewayv2_integration" "alb_integration" {
   depends_on         = [aws_apigatewayv2_api.api_gateway]
   api_id             = aws_apigatewayv2_api.api_gateway.id
   integration_type   = "HTTP_PROXY"
-  connection_id      = aws_apigatewayv2_vpc_link.vpc_link.id
-  connection_type    = "VPC_LINK"
+  connection_type    = "INTERNET"
   integration_method = "ANY"
-  integration_uri    = data.terraform_remote_state.geral.outputs.alb_http_arn
+  integration_uri    = "http://${data.terraform_remote_state.geral.outputs.alb_dns}"
 }
 
 
